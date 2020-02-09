96f5a2ae8738d3d18306655e69f2874179711836
sgal996 <galacsim@gmail.com>
2020-02-09 20:27:01 +0100

    order controller

--- src/main/java/com/webshop/shop/controller/OrderController.java
+++ src/main/java/com/webshop/shop/controller/OrderController.java
@@ -5,17 +5,24 @@ import com.webshop.shop.dto.OrderDto;
 import com.webshop.shop.dto.ProductDto;
 import com.webshop.shop.model.Order;
 import com.webshop.shop.model.Product;
+import com.webshop.shop.model.User;
 import com.webshop.shop.repository.OrderRepository;
+import com.webshop.shop.security.CustomUserDetails;
 import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.ws.Response;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import com.webshop.shop.dto.OrderDto;
 
 @RestController
@@ -23,9 +30,14 @@ import com.webshop.shop.dto.OrderDto;
 public class OrderController {
 
     private OrderRepository orderRepository;
+    private Principal principal;
+
+
+    private Authentication authentication;
 
     public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
+
     }
 
     @PostMapping("/saveorder")
@@ -46,13 +58,14 @@ public class OrderController {
         }
         order.setProducts(productList);
         order.setCreatedAt(new Date());
+        CustomUserDetails user = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        order.setUserId(user.getId().toString());
 
-        order.setUserId("1");
         orderRepository.save(order);
 
         return ResponseEntity.ok("Narudžba uspješno izvršena!!");
     }
-    }
+}
 
 
 
