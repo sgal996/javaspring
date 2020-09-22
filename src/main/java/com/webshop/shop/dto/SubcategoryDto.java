a46698614e337c6256ce199a341a321f3dc04f87
sgal996 <galacsim@gmail.com>
2020-09-22 13:00:14 +0200

    added functionalities

---
+++ src/main/java/com/webshop/shop/dto/SubcategoryDto.java
@@ -0,0 +1,13 @@
+package com.webshop.shop.dto;
+
+public class SubcategoryDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
